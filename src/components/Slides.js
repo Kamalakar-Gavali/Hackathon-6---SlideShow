f483531795654446fa8e71a552aa0febda0e0df3
kamalakar <kg@gmail.com>
2020-11-21 20:02:31 +0530

    done

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -83,6 +83,7 @@ const Slides=(props)=>{
         }
         else
         {
+            
             idRestart.current.style.pointerEvents='none';
             idPrev.current.style.pointerEvents='none';
         }
