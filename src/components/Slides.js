09ec742f8e278be9dba1d073f4687399e67ad7bb
kamalakar <kg@gmail.com>
2020-11-21 19:29:03 +0530

    done

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -12,71 +12,89 @@ const Slides=(props)=>{
 
 
     const next=()=>{
-        console.log(slideIndex);
-        if(slideIndex==slideSize-1)
+        console.log("In Next Function");
+        setSildeIndex(slideIndex+1);
+        /*if(slideIndex==slideSize-1)
         {
-            idNext.current.style.disabled='true';
+            idNext.current.style.pointerEvents='none';
             //idNext.current.style.pointerEvents='none';
         }
         else
         {
          //   idNext.current.style.pointerEvents='auto';
-         idNext.current.style.enabled='true';
+           idNext.current.style.pointerEvents='auto';
             setSildeIndex(slideIndex+1);
-        }
+        }*/
+        
     }
     const restart=()=>{
-        
+        console.log("In Restart Function");
         if(slideIndex!=0)
         {
-         setSildeIndex(0);
+        setSildeIndex(0);
         }
-        else{
-            idNext.current.style.enabled='true';
-        }
-    }
-    const prev=()=>{
-        //console.log(slideIndex);
-        if(slideIndex==0)
+        /*if(slideIndex!=0)
         {
-            //idPrev.current.style.pointerEvents='none';
-            idPrev.current.style.disabled='true';
+          idNext.current.style.pointerEvents='auto';
+         setSildeIndex(0);
         }
         else
         {
-            //idPrev.current.style.pointerEvents='auto';
-            idPrev.current.style.enabled='true';
-            setSildeIndex(slideIndex-1);
-        }
+            idNext.current.style.pointerEvents='none';
+        }*/
+        
     }
-    /*useEffect(()=>{
+    const prev=()=>{
+        console.log("In Prev Function");
+        //setSildeIndex(slideIndex-1);
+        if(slideIndex!=0)
+        {
+            //idPrev.current.style.pointerEvents='auto';
+            setSildeIndex(slideIndex-1);
+            //idPrev.current.style.pointerEvents='none';
+            //idPrev.current.style.disabled='true';
+        }
+        /*else
+        {
+            idPrev.current.style.pointerEvents='none';
+        }*/
+        
+    }
+    useEffect(()=>{
         if(slideIndex==0)
         {
             //idPrev.current.style.pointerEvents='none';
-            idPrev.current.style.disabled='true';
+            idPrev.current.style.pointerEvents='none';
+            idRestart.current.style.pointerEvents='none';;
         }
         if(slideIndex==slideSize-1)
         {
-            idNext.current.style.disabled='true';
+            idNext.current.style.pointerEvents='none';
             //idNext.current.style.pointerEvents='none';
+        }
+        else{
+            idNext.current.style.pointerEvents='auto';
         }
         if(slideIndex>0)
         {
-            idRestart.current.style.enabled="true"
+            idRestart.current.style.pointerEvents='auto';
+            
+            idPrev.current.style.pointerEvents='auto';
         }
         else
         {
-            idRestart.current.style.disabled="true"
+            idRestart.current.style.pointerEvents='none';
+            idPrev.current.style.pointerEvents='none';
         }
 
-    },[slideIndex])*/
+    },[slideIndex])
     return(
         <>
      <h1 data-testid="title">{props.slides[slideIndex].title}</h1>
      <p data-testid="text">{props.slides[slideIndex].text}</p>
+     <button data-testid="button-prev" onClick={prev} ref={idPrev} style={{pointerEvents:"none"}}> Prev</button>
+     <button data-testid="button-restart" ref={idRestart} onClick={restart} style={{pointerEvents:"none"}}>Restart</button>
      <button data-testid="button-next" ref={idNext} onClick={next}>Next</button>
-     <button data-testid="button-restart" ref={idRestart} onClick={restart} style={{disabled:true}}>Restart</button>
-     <button data-testid="button-prev" onClick={prev} ref={idPrev} style={{disabled:true}}> Prev</button>
      
      </>
     )
